2c5b75441aafa1cab0250cc3bd8b0a3902362345
Genie <genie0320@gmail.com>
2021-11-11 17:04:15 +0900

    Router & Pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import Navbar from './components/Navbar';
+import Home from './pages/home/Home'
+import Create from './pages/create/Create'
+import Recipe from './pages/recipes/Recipe'
+import Search from './pages/search/Search'
+
 import './App.css'
 
 function App() {
   return (
     <div className="App">
-      
+
+      <BrowserRouter>
+        <Navbar />
+        <Switch>
+          {/* Route !!! not Router !!! */}
+          <Route exact path='/'>
+            <Home />
+          </Route>
+          <Route path='/create'>
+            <Create />
+          </Route>
+          <Route path='/recipe/:id'>
+            <Recipe />
+          </Route>
+          <Route path='/search'>
+            <Search />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import { Link, NavLink } from 'react-router-dom'
+
+import './Navbar.css'
+
+export default function Navbar() {
+    return (
+        <div className='navbar'>
+            <nav>
+                <Link to='/' className='brand'>
+                    <h1 >Cooking Genie</h1>
+                </Link>
+                <Link to='/create'>New Recipe</Link>
+            </nav>
+        </div>
+    )
+}
