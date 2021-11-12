54641419f9a7d8080d548427fed7e0687d82fcaa
Genie <genie0320@gmail.com>
2021-11-12 22:59:47 +0900

    .

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,9 +1,14 @@
+import { useFetch } from '../../hooks/useFetch'
+
 import './Home.css'
 
 export default function Home() {
+    const { data: recipes, isPending, error } = useFetch('http://localhost:3000/recipes')
+
     return (
         <div>
-            Home
+            Home.
+            Today is so... tired.
         </div>
     )
 }
