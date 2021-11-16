5c5438c8cfd1bf2bca5e79812320f5998d7daf4b
Genie <genie0320@gmail.com>
2021-11-16 17:06:17 +0900

    Add recipe page

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -1,9 +1,35 @@
+import { useState } from 'react'
 import './Create.css'
 
 export default function Create() {
+    const [title, setTitle] = useState('')
+    const [method, setMethod] = useState('')
+    const [cookingTime, setCookingTime] = useState('')
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log(title, method, cookingTime)
+
+    }
     return (
-        <div>
-            Create
+        <div className='addRecipe'>
+            <h2>Add new recipe</h2>
+
+            <form onSubmit={handleSubmit}>
+                <label>
+                    <span>Recipe title</span>
+                    <input type='text' onChange={(e) => { setTitle(e.target.value) }} value={title} required />
+                </label>
+                <label>
+                    <span>Cooking time</span>
+                    <input type="number" onChange={(e) => { setCookingTime(e.target.value) }} value={cookingTime} required />
+                </label>
+                <label>
+                    <span>Method</span>
+                    <textarea onChange={(e) => { setMethod(e.target.value) }} value={method} required ></textarea>
+                </label>
+                <button className='submit'>submit</button>
+            </form>
         </div>
     )
 }
