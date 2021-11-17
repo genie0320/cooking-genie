163456d4aec3b661ff1f676da2a237060b4d8051
Genie <genie0320@gmail.com>
2021-11-17 12:01:35 +0900

    Add Ingredients in create page - useRef() - Lots of useState()

--- README.md
+++ README.md
@@ -3,3 +3,14 @@
 * Save input data (Local)
 * Keyword search
 * Dark theme
+
+# React hooks
+
+# React router hooks
+## useLocation() 
+The useLocation hook returns the location __object__ that represents the __current URL__. 
+It's just like a useState returns a new location whenever the URL changes.
+
+## useParams()
+useParams returns an __object__ of key/value pairs of URL parameters. 
+
--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -5,11 +5,18 @@ export default function Create() {
     const [title, setTitle] = useState('')
     const [method, setMethod] = useState('')
     const [cookingTime, setCookingTime] = useState('')
+    const [ingItems, setIngItems] = useState('')
+    const [newIng, setNewIng] = useState([])
 
     const handleSubmit = (e) => {
         e.preventDefault()
         console.log(title, method, cookingTime)
 
+    }
+
+    const handleClick = (item) => {
+
+        console.log(item)
     }
     return (
         <div className='addRecipe'>
@@ -18,15 +25,22 @@ export default function Create() {
             <form onSubmit={handleSubmit}>
                 <label>
                     <span>Recipe title</span>
-                    <input type='text' onChange={(e) => { setTitle(e.target.value) }} value={title} required />
+                    <input type='text' onChange={(e) => { setTitle(e.target.value) }} value={title} requir ed />
                 </label>
                 <label>
-                    <span>Cooking time</span>
-                    <input type="number" onChange={(e) => { setCookingTime(e.target.value) }} value={cookingTime} required />
+                    <span>Ingredients</span>
+                    <div className="ingredients">
+                        <input type="text" />
+                        <button className="btn" onClick={console.log('hi')}>add</button>
+                    </div>
                 </label>
                 <label>
                     <span>Method</span>
-                    <textarea onChange={(e) => { setMethod(e.target.value) }} value={method} required ></textarea>
+                    <textarea onChange={(e) => { setMethod(e.target.value) }} value={method} requi red ></textarea>
+                </label>
+                <label>
+                    <span>Cooking time</span>
+                    <input type="number" onChange={(e) => { setCookingTime(e.target.value) }} value={cookingTime} requ ired />
                 </label>
                 <button className='submit'>submit</button>
             </form>
