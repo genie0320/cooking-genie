b9c54a1887da59f9b52ffbdb4de1ac1ea86af3de
Genie <genie0320@gmail.com>
2021-11-18 23:21:15 +0900

    Add ingredients

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -1,15 +1,28 @@
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 import './Create.css'
 
 export default function Create() {
     const [title, setTitle] = useState('')
     const [method, setMethod] = useState('')
     const [cookingTime, setCookingTime] = useState('')
+    const [ingredients, setIngredients] = useState([])
+    const [newIng, setNewIng] = useState('')
+    const ingInput = useRef(null)
 
     const handleSubmit = (e) => {
         e.preventDefault()
-        console.log(title, method, cookingTime)
+        console.log(title, cookingTime, ingredients, method)
+    }
 
+    const handleClick = (e) => {
+        e.preventDefault()
+
+        const ing = newIng.trim()
+        if (ing && !ingredients.includes(ing)) {
+            setIngredients(prevIng => [...prevIng, newIng])
+            setNewIng('')
+            ingInput.current.focus()
+        }
     }
     return (
         <div className='addRecipe'>
@@ -24,6 +37,16 @@ export default function Create() {
                     <span>Cooking time</span>
                     <input type="number" onChange={(e) => { setCookingTime(e.target.value) }} value={cookingTime} required />
                 </label>
+
+                <label>
+                    <span>Ingredients</span>
+                    <div className="ing">
+                        <input type="text" onChange={(e) => setNewIng(e.target.value)} value={newIng} ref={ingInput} />
+                        <button onClick={handleClick}>add</button>
+                    </div>
+                </label>
+                <span>Ingredients :  {ingredients.map((ing) => <em key={ing}>{ing}, </em>)}</span>
+
                 <label>
                     <span>Method</span>
                     <textarea onChange={(e) => { setMethod(e.target.value) }} value={method} required ></textarea>
