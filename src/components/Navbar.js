57bd84d204fc4d857c632e728098fa31249b7f95
Genie <genie0320@gmail.com>
2021-11-25 23:11:14 +0900

    Add searchbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom'
+import Searchbar from './Searchbar'
 
 import './Navbar.css'
 
@@ -9,6 +10,7 @@ export default function Navbar() {
                 <Link to='/' className='brand'>
                     <h1 >Cooking Genie</h1>
                 </Link>
+                <Searchbar />
                 <Link to='/create'>New Recipe</Link>
             </nav>
         </div>
---
+++ src/components/Searchbar.js
@@ -0,0 +1,37 @@
+import { useState } from 'react'
+import { useHistory } from 'react-router-dom'
+// 디폴트는 react-router 였는데 -dom을 붙였다. 뭐가 다른거지?
+// 새로운 react-router-dom 버전에서는 useNavigate() 로 바뀌었다. 
+
+import './Searchbar.css'
+
+export default function Searchbar() {
+    const [keyword, setKeyword] = useState('')
+    const history = useHistory()
+
+    const handleInput = (e) => {
+        e.preventDefault()
+
+        setKeyword(e.target.value)
+    }
+
+    const handleSearch = (e) => {
+        e.preventDefault()
+        // ' ' 가 아니다. ` ` 다!
+        history.push(`/search?q=${keyword}`)
+    }
+    return (
+        <div>
+            <div className="searchbar">
+                <form onSubmit={handleSearch}>
+                    <label htmlFor="search">Search : </label>
+                    <input
+                        type="text"
+                        id='search'
+                        onChange={handleInput}
+                    />
+                </form>
+            </div>
+        </div>
+    )
+}
