8a90ca88cc0c06b9aed3b59ba1d6ad2416a72e37
Genie <genie0320@gmail.com>
2021-11-26 17:01:26 +0900

    caution

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,13 @@
 import { Link } from 'react-router-dom'
 import Searchbar from './Searchbar'
 import { useContext } from 'react'
-
 import { ThemeContext } from '../context/ThemeContext'
 
 import './Navbar.css'
 
 export default function Navbar() {
     const { color } = useContext(ThemeContext)
+    // ContextProvider 가 아닌, useContext를 사용하는 것에 주의.
 
     return (
         <div className='navbar' style={{ background: color }}>
