5afe609671f4bac7e3a76299b740a0ac74b8bca1
Genie <genie0320@gmail.com>
2021-11-26 17:58:39 +0900

    useReducer for theme color chagne

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,11 +5,16 @@ import { useTheme } from '../hooks/useTheme'
 import './Navbar.css'
 
 export default function Navbar() {
-    const { color } = useTheme()
+    const { color, colorChange } = useTheme()
+
+    const handleNavClick = () => {
+        console.log('clicked!')
+        colorChange('pink')
+    }
 
     return (
         <div className='navbar' style={{ background: color }}>
-            <nav>
+            <nav onClick={handleNavClick}>
                 <Link to='/' className='brand'>
                     <h1 >Cooking Genie</h1>
                 </Link>
--- src/context/ThemeContext.js
+++ src/context/ThemeContext.js
@@ -1,11 +1,28 @@
-import { createContext } from "react";
+import { createContext, useReducer } from "react";
 
 export const ThemeContext = createContext()
 
+const colorReducer = (state, action) => {
+    switch (action.type) {
+        case 'CHANGE_COLOR':
+            return { ...state, color: action.color }
+        default:
+            return state
+    }
+}
+
 export function ThemeProvider({ children }) {
 
+    const [state, dispach] = useReducer(colorReducer, {
+        color: 'blue'
+    })
+
+    const colorChange = (color) => {
+        dispach({ type: 'CHANGE_COLOR', payload: color })
+    }
+
     return (
-        <ThemeContext.Provider value={{ color: 'blue' }}>
+        <ThemeContext.Provider value={{ ...state, colorChange }}>
             {children}
         </ThemeContext.Provider>
     )
